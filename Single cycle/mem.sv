42ae2a1586bd08290f503ff5ab78a190525198e1
Farwa Ali <157650142+FarwaNaqvi@users.noreply.github.com>
2025-03-04 10:03:49 +0500

    mem.sv

--- Single cycle/mem.sv
+++ Single cycle/mem.sv
@@ -1,11 +1,11 @@
-module wa_ba_mem(
+module mem(
     //input clk, write_en,
     input [10:0] address,
     //input [7:0] data_in,
     output [31:0] data_out
 );
     
-// 2KB memory, word-aligned and byte-addressable
+
 // Case 1: I can use same file for read and write opertaion in sense I know.
 // Case 2: I Use here ditinict files for both.
 
